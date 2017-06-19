24ed62c26874f8a4f255134a650a118df473a5c8
Jen Ayala <jenayala1@gmail.com>
2017-06-19 11:11:19 -0500

    Adding first array exercise without for loop & for each loop iteration - named iterating1.js, did not initially save the first exercise

---
+++ public/js/iterating1.js
@@ -0,0 +1,33 @@
+(function(){
+    "use strict";
+
+
+
+    // TODO: Create an array of 4 people's names using literal array notation, in a variable called 'names'.
+
+    var names = ["Vincent", "Ian", "Calista", "Giada"];
+			
+
+    // TODO: Create a log statement that will log the number of elements in the names array.
+
+    	console.log("There are " + names.length + " names in the names array");
+
+    // TODO: Create log statements that will print each of the names array elements individually
+
+        console.log('The first name is: ' + names[0]);
+        console.log('The second name is: ' + names[1]);
+        console.log('The third name is: ' + names[2]);
+        console.log('The fourth name is: ' + names[3]);
+
+
+  	});
+
+    //ARRAY BONUS
+    //Create a simple mad libs game which prompts the user to provide parts of speech to complete missing 
+    //words of phrases for 3-4 silly sentences.  Add each word or phrase from each prompt to single array.
+    //Once a user has entered all the Mad Libs, alert the results.
+    //Use a confirm to play again or not. Validate user input to prevent blank entries.
+    //At the very least, wrap your program in an IIFE function. Consider organizing your code into smaller
+    //functions.
+
+    //Bonus answer: MAD LIBS//
