a3ffc7aad12d403f019ee4a5f98db3be2d567bf0
Jen Ayala <jenayala1@gmail.com>
2017-06-23 12:42:40 -0500

    Revisions to calculator javascript to add in functions, revisions to HTML to support functions, revisions to CSS for styling

--- public/js/calculator.js
+++ public/js/calculator.js
@@ -7,110 +7,79 @@
 var rightinput = document.getElementById("rightinput");
 var moperator = document.getElementById("moperator");
 var leftinput = document.getElementById("leftinput");
-var ddecimal = document.getElementById("bdecimal");
-var bpercent = document.getElementById("bpercent");
-
-//2. command to begin cursor in the left operand field & disallow entry in next fields until populated
-//3. function to retrieve input from HTML buttons pushed (0-9), returned as numerical values into the left operand field
-	//may need an if/else or true statment to determine which input field to place the value
-
-var zero = document.getElementById("zero");
-	zero.addEventListener("click", function() {
-	leftinput.value = 0;
-	});
-
-var one = document.getElementById("one");
-	one.addEventListener("click", function() {
-	leftinput.value = 1;
-	});
-
-var two = document.getElementById("two");
-	two.addEventListener("click", function() {
-	leftinput.value = 2;
-	});
-
-var three = document.getElementById("three");
-	three.addEventListener("click", function() {
-	leftinput.value = 3;
-	});
-
-var four = document.getElementById("four");
-	four.addEventListener("click", function() {
-	leftinput.value = 4;
-	});
-
-var five = document.getElementById("five");
-	five.addEventListener("click", function() {
-	leftinput.value = 5;
-	});
-
-var six = document.getElementById("six");
-	six.addEventListener("click", function() {
-	leftinput.value = 6;
-	});
-
-var seven = document.getElementById("seven");
-	seven.addEventListener("click", function() {
-	leftinput.value = 7;
-	});
-
-var eight = document.getElementById("eight");
-	eight.addEventListener("click", function() {
-	leftinput.value = 8;
-	});
-
-var nine = document.getElementById("nine");
-	nine.addEventListener("click", function() {
-	leftinput.value = 9;
-	});
-
-
-//4. function to retrieve input from HTML buttons pushed (+, -, *, /), 
-//returned as mathematical values in the middle operator field
-var bmultiply = document.getElementById("bmultiply");
-	bmultiply.addEventListener("click", function() {
-	moperator.value = "multiply";
-	});
-
-var bplus = document.getElementById("bplus");
-	bplus.addEventListener("click", function() {
-	moperator.value = "add";
-	});
-
+var number = document.getElementsByClassName("number");
+var operator = document.getElementsByClassName("operator");
+var calculate
+var answer;
 var bequal = document.getElementById("bequal");
-	bequal.addEventListener("click", function() {
-	moperator.value = "equal";
-	});
+var clear = document.getElementById("clear");
 
-// var bdivide = document.getElementById("bdivide");
-// 	bdivide.addEventListener("click", function() {
-// 	moperator.value = "divide";
-// 	});
+//for loop thru operators & numbers to set event listener by class
+for (var i = 0; i < number.length; i++) {
+	number[i].addEventListener("click", buttonHandler);
+}
 
-var bsubtract = document.getElementById("bsubtract");
-	bsubtract.addEventListener("click", function() {
-	moperator.value = "subtract";
-	});	
+for (var i = 0; i < operator.length; i++) {
+	operator[i].addEventListener("click", buttonHandler);
+}
 
-//var calculate = document.get
+//establish function for each input using if/else statement, beginning with operator
+function buttonHandler() {
+	if (this.className.includes("operator")) {
+		moperator.setAttribute("value", this.value);
+		console.log(moperator);
+	} else if (moperator.value != "", leftinput.value !="") {
+		var displayInfo = rightinput.getAttribute("value");
+		displayInfo += this.value; 
+		rightinput.setAttribute("value", displayInfo);
+		console.log(displayInfo);
+	} else if (this.className.includes('number')) {
+		var displayInfo = leftinput.getAttribute("value");
+		displayInfo += this.value; 
+		leftinput.setAttribute("value", displayInfo);
+		console.log(displayInfo);
+	}
 
-
-//5. Function to retrieve input from HTML buttons pushed (0-9), returned as numerical values in the right operand field
-	//consider using append or concatenation to add the values to the correct input box
-// 1st Solution:
-// function AddRightInput (getDig) {
-// 	if (var leftinput == null);
+}
 	
-// 	}, else 
-// 		function (calculate) {
-	
-// 	}, else ( == 'bplus') {
+//clear each value, by element id
+clear.addEventListener("click", function() {
+	leftinput.setAttribute("value", "");
+	moperator.setAttribute("value", "");
+	rightinput.setAttribute("value", "");
 
-//6. Function to calculate all three input fields (left to right) when the "=" button is pushed
-//7. Function to clear the fields when the "C" button is pushed 
-var clear
-//		Complete after the add input functions are complete - will be referenced 
-//		(possibly) as a removeEventListener to the addinput
+});
 
 
-//8. functions to disallow invalid entries in the selected fields 
+//calculate function for "=":
+bequal.addEventListener("click", function() {
+	var leftinputNumber = parseFloat(leftinput.getAttribute("value")); //converting string to numerical value
+	var rightinputNumber = parseFloat(rightinput.getAttribute("value"));
+
+	switch(moperator.value) {
+		case "+":
+		answer = leftinputNumber + rightinputNumber;
+		break;
+
+		case "-":
+		answer = leftinputNumber - rightinputNumber;
+		break;
+
+		case "*":
+		answer = leftinputNumber * rightinputNumber;
+		break;
+
+		case "/":
+		answer = leftinputNumber / rightinputNumber;
+		break;
+	}
+	leftinput.setAttribute("value", answer);
+	rightinput.setAttribute("value", "");
+	moperator.setAttribute("value", "");
+	console.log(leftinput.value);
+});
+
+
+
+
+
