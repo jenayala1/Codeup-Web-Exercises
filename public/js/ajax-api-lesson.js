299d28b035537c5d26fed6662124e99eeccde91e
Jen Ayala <jenayala1@gmail.com>
2017-07-05 16:56:25 -0500

    Adding AJAX Death Star refactor exercise, lesson notes, and revisions to AJAX blog

---
+++ public/js/ajax-api-lesson.js
@@ -0,0 +1,16 @@
+//========== AJAX API REQUEST LESSON NOTES 07/05/2017 ============
+
+//=== AGENDA ===//
+//-- AJAX Blog walk-thru
+// -- More API Work - weather API
+
+
+
+//GIT HUB ERRORS - push rejected because work is not local
+
+//--to pull in work pushed to git hub that is being worked on collaboratively
+git pull origin master
+
+
+
+//AJAX STORE walk-thru
