9bb0133d3982c3a3a7ee4ac99102c3578004d666
Jen Ayala <jenayala1@gmail.com>
2017-07-07 16:52:59 -0500

    Work on weather API exercise - added map & marker, pending code to retrieve position coordinates when marker is dragged to new location

--- public/js/weather-api.js
+++ public/js/weather-api.js
@@ -1,41 +1,54 @@
 (function() {
-        "use strict";
+    "use strict";
 
-        var coordinates = {lat: 29.428074, lng: -98.489158}; 
-        var location = "San Antonio, TX 78205"; 
-
-//---#1 ---- Set map options -----
+        var coordinates = {lat: 36.600238, lng: -121.894676}; 
+        var location = "Monterey, CA 93944"; 
+                    
         var mapOptions = {
-            // Set the zoom level
-            zoom: 19,
-
-//----#2 -----Sets center of the map 
+            zoom: 12,
             center: {
-                lat:  29.428074,
-                lng: -98.489158
+                lat:  36.600238,
+                lng: -121.894676
             },
 
-         mapTypeId: google.maps.MapTypeId.HYBRID
+            mapTypeId: google.maps.MapTypeId.HYBRID
 
         };
+                    
+        var map = new google.maps.Map(document.getElementById("map-canvas"), mapOptions);
+        var address = "1759 Lewis Rd, Monterey, CA 93944";
 
+        var geocoder = new google.maps.Geocoder();
+            geocoder.geocode({address: address}, function(results, status) {
+                if (status == google.maps.GeocoderStatus.OK) {
+                    map.setCenter(results[0].geometry.location);
+                                
+                } else {
+                    alert("Oops " + status);
+                }
+            });
 
-// ----#3 -------Render map
-var map = new google.maps.Map(document.getElementById("map-canvas"), mapOptions);
-var address = "311 E Travis St, San Antonio, TX 78205";
+        var marker = new google.maps.Marker({
+             position: coordinates,
+             map: map,
+             draggable: true,
+             animation: google.maps.Animation.DROP,
+        });
 
-var geocoder = new google.maps.Geocoder();
-    geocoder.geocode({address: address}, function(results, status) {
-        if (status == google.maps.GeocoderStatus.OK) {
-                map.setCenter(results[0].geometry.location);
+        $("marker").click(function(getWeatherOption){
+            lat = $("#lat").val();
+            lon = $("#lon").val();
+             $("#forecast").html("");
+             getWeatherOption();
+        });
 
-            } else {
-                alert("Oops, something went wrong. " + status);
-            }
+        google.maps.event.addListener(marker, 'draggable', function (event) {
+            document.getElementById("lat").value = this.getPosition().lat();
+            document.getElementById("lon").value = this.getPosition().lng();
+            console.log(lat + lng);
+        });
 
-        })
-
-"http://tile.openweathermap.org/map/temp_new/19/{x}/{y}.png?appid=40ba7777b52dad7589a9efb71eceb30a"
+})();
 
 
 ///---_#4-----Create a marker on your map of the exact location of your favorite restaurant and adjust the zoom to allow for best viewing distance.
@@ -43,19 +56,12 @@ var geocoder = new google.maps.Geocoder();
 ///----#6-----Info windows can contain basic HTML, not just plain text. Add some addition information about your restaurant to the info window such as why it is your favorite, dishes you like, etc.
 ///----#7-----Research Google Maps Events. Make sure the info window does not display until the marker has been clicked on.    
 
-    var marker = new google.maps.Marker({
-            position: location,
-            map: map,
-            draggable: true,
-            animation: google.maps.Animation.DROP,
-    });
+    // 
 
-    var infowindow = new google.maps.InfoWindow({
-        content: "Travis Park" + "  311 E Travis St, San Antonio, TX 78216  ",
-        maxWidth: 200
-    });
+    // var infowindow = new google.maps.InfoWindow({
+    //     content: "1759 Lewis Rd" + "  Monterey, CA 93944  ",
+    //     maxWidth: 200
+    // });
 
     
-    // marker.addListener('click', function(){
-    //     infowindow.open(map, marker);
-    // });
+    
