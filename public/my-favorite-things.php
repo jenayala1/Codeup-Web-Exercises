c9d07f6a6e94f9ab298cbef1e09ad909ee96c359
Jen Ayala <jenayala1@gmail.com>
2017-07-31 13:03:21 -0500

    MVC exercises & lesson notes

---
+++ public/my-favorite-things.php
@@ -0,0 +1,34 @@
+<?php
+
+	$favoriteThings = ['writing', 'music', 'art', 'my kids', 'electronics', 'art supplies', 'dogs', 'snakes', 'tarantulas', 'reading'];
+
+?>
+
+<!DOCTYPE html>
+<html>
+		<head>
+			<meta charset="utf-8">
+			<title>My Favorite Things</title>
+			<link href='https://fonts.google.com/specimen/Play' rel='stylesheet' type'text/css'>
+			<link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+	 	<link href="css/mft.css" rel="stylesheet">
+
+		</head>
+		<body>
+			<h1> Just a list!</h1>
+			<table>
+				<thead>
+					<tr> My Favorite Things</tr>
+				</thead>
+				<tbody>	
+					<?php foreach($favoriteThings as $thing) { ?>
+					<tr> 
+						<td><?php echo $thing ?>
+						</td>
+					</tr>
+						<?php } ?> 
+				</tbody>
+			</table>
+
+		</body>
+</html>
---
+++ public/server-name-generator.php
@@ -0,0 +1,32 @@
+<?php
+	$adjectives = ['humourous', 'outgoing', 'nice', 'attentive', 'scary', 'loud', 'annoying', 'engaging', 'interesting', 'intelligent'];
+
+	$nouns = ['house', 'car', 'music', 'art', 'people', 'electronics', 'supplies', 'dog', 'python', 'tarantula'];
+
+		function randomArrayElement($array)
+	{
+		return $array[rand(0, count($array) -1)];
+	}
+		
+	function randomServerName($arr1, $arr2)
+	{
+		return randomArrayElement($arr1) . randomArrayElement($arr2);
+	}
+		
+	
+
+?>
+
+<!DOCTYPE html>
+<html>
+	<head>
+		<title>server-name-generator</title>
+	</head>
+	<body>
+		<h1> Just a list!</h1>
+
+		<h1><?php echo (randomServerName($adjectives, $nouns)); ?></h1>
+	
+	</body>
+</html>
+
