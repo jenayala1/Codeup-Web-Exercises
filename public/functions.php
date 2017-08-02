4c92cb1d9923776e506acbe3d8611bf5f457afe2
Jen Ayala <jenayala1@gmail.com>
2017-08-02 17:01:32 -0500

    Revisions to functions

--- public/functions.php
+++ public/functions.php
@@ -9,22 +9,25 @@
 
 function inputHas($key)
 {
-	
+
 	return isset($_REQUEST[$key]);
 }
-
 
 
 function inputGet($key)
 {
-
-	return isset($_REQUEST[$key]);
-
+	if(inputHas($key)) {
+		return $_REQUEST[$key];
+	} else {
+		return $default;
+	}
 }
-
 
 
 function escape($input)
 {
-	return isset($_REQUEST[$input]);
+	if(!is_string($input)){
+		return false;
+	}
+	return htmlspecialchars(strip_tags($input));
 }
